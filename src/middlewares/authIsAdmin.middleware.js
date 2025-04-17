df47ef4620b89e70a64b0033461df33ef37b2c23
José Willames <willames5480@gmail.com>
2025-04-17 18:14:29 -0300

    Fixed issue with deleteProduct function. When it received a null ID value, it returned a status 200 with the message that the product was successfully deleted.

--- src/middlewares/authIsAdmin.middleware.js
+++ src/middlewares/authIsAdmin.middleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { verifyResetToken } = require("../utils/jwt.utils")
+const { verifyResetToken } = require("../utils/jwt.utils");
 
 const authIsAdminMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
@@ -12,48 +12,49 @@ const authIsAdminMiddleware = (req, res, next) => {
   }
 
   try {
-      const decoded = verifyResetToken(token) 
+    const decoded = verifyResetToken(token);
     // const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY); //decodes the token received in the request
-    if (!decoded || typeof decoded.isAdmin === "undefined" || !decoded.isAdmin) {
-      return res
-        .status(401)
-        .json({
-          message:
-            "Access denied. You don't have permission. Only administrators can register a product",
-        });
+    if (
+      !decoded ||
+      typeof decoded.isAdmin === "undefined" ||
+      !decoded.isAdmin
+    ) {
+      return res.status(401).json({
+        message:
+          "Access denied. You don't have permission. Only administrators can register a product",
+      });
     }
 
-    req.user = decoded;  //add the authenticated user information to the request
-    
-    next();
+    req.user = decoded; //add the authenticated user information to the request
 
+    next();
   } catch (error) {
-    console.log(error)
-    res.status(403).json({ message: "Invalid token or expired token!", error});
+    res.status(403).json({ message: "Invalid token or expired token!", error });
   }
 };
+
 const verifyTokenMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
-  if(!token) {
-    return res.status(401).json({ message: "Token not provided!"})
+  if (!token) {
+    return res.status(401).json({ message: "Token not provided!" });
   }
+
   try {
-    const decoded = verifyResetToken(token)
-    
+    const decoded = verifyResetToken(token);
+
     // const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
-    if(!decoded || decode.error) {
+    if (!decoded || decode.error) {
       return res.status(403).json({
-        message: "Invalid token or expired token!", details: decoded?.error || "Token verification failed!"
-      })
+        message: "Invalid token or expired token!",
+        details: decoded?.error || "Token verification failed!",
+      });
     }
     req.user = decoded;
     next();
-
   } catch (error) {
-    res.status(403).json({ message:"Invalid token or token expire!"})
+    res.status(403).json({ message: "Invalid token or token expire!" });
   }
-}
+};
 
-
-module.exports = {authIsAdminMiddleware, verifyTokenMiddleware}
+module.exports = { authIsAdminMiddleware, verifyTokenMiddleware };
