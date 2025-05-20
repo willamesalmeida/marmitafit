72b071d48dd21f9c4ea98cafe5059326efbbf96e
José Willames <willames5480@gmail.com>
2025-05-20 11:32:36 -0300

    feat(admin): add pagination and sorting to order search filter in the admin panel

--- src/controllers/adminOrder.controller.js
+++ src/controllers/adminOrder.controller.js
@@ -5,13 +5,16 @@ class AdminOrderController {
     try {
       const filter = req.query || {};
 
-      const orders = Object.keys(filter).length === 0 ? await OrderService.getAllOrders() : await OrderService.getFilteredOrders(filter)
+      /*const orders = Object.keys(filter).length === 0 
+      ? await OrderService.getAllOrders() 
+      : await OrderService.getFilteredOrders(filter)
 
-      res.status(200).json({ orders });
-      /* const orders = await OrderService.getAllOrders();
+      res.status(200).json({ orders }); */
+
+      const orders = await OrderService.getFilteredOrders(filter);
       res.status(200).json({
         orders
-      }) */
+      }) 
     } catch (error) {
       next(error);
     }
