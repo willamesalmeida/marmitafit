20940e4e37953b61fad3bc6346096e80f02690f9
José Willames <willames5480@gmail.com>
2025-12-17 17:33:18 -0300

    Complete Refactoring of refreshToken: 7-day expiration, automatic cleanup, support for deviceId, and revocation by device

--- src/services/refreshToken.service.js
+++ src/services/refreshToken.service.js
@@ -1,11 +1,21 @@
 // const prisma = require("../config/database")
 const { PrismaClient } = require("@prisma/client");
 const dayjs = require("dayjs");
-
 //class handler error
 const AppError = require("../utils/errorHandler.util");
 
 const prisma = new PrismaClient();
+
+const MAX_ACTIVE_TOKENS = 5;
+/**
+ * Saves a new refresh token to the database, revoking old tokens from the same device and limiting the total number of active tokens.
+ * @param {string} userId - ID of the user
+ * @param {string} token - Refresh token JWT
+ * @param {Date} expiresIn - Expiration date of the token
+ * @param {string|null} deviceId - ID of the device (optional)
+ * @returns {Promise<Object>} - Object of the refresh token saved
+ * @throws {AppError} - If an error occurs during saving process token or exceeding the limit
+ */
 
 /* const saveRefreshToken = async (userId, token, expiresIn) => {
   
@@ -44,12 +54,45 @@ const prisma = new PrismaClient();
 
 const saveRefreshToken = async (userId, token, expiresIn, deviceId = null) => {
   try {
-    if(deviceId) {
+    // revoked the old tokens for the same user and device, if deviceId is provided
+
+    if (deviceId) {
       await prisma.refreshToken.updateMany({
-        where: { userId, deviceId, isRevoked: false},
-        data: {isRevoked: true},
+        where: { userId, deviceId, isRevoked: false },
+        data: { isRevoked: true },
       });
     }
+
+    //Verify the number of active tokens for the user
+    const activeTokens = await prisma.refreshToken.count({
+      where: {
+        userId,
+        isRevoked: false,
+        expiresIn: { gte: new Date() }, // only count tokens that are not revoked and not expired
+      }
+    })
+
+    if (activeTokens >= MAX_ACTIVE_TOKENS) {
+      //Revokes the oldest token if the limit is reached
+      const oldestToken = await prisma.refreshToken.findFirst({
+        where: {
+          userId,
+          isRevoked: false,
+          expiresIn: { gte: new Date() }, // only consider active tokens
+        },
+        orderBy: { createdAt: 'asc' }
+      })
+
+      if (oldestToken) {
+        await prisma.refreshToken.update({
+          where: { id: oldestToken.id },
+          data: { isRevoked: true },
+        });
+      } else {
+        throw new AppError("Unable to revoke oldest token", 500);
+      }
+    }
+    // create a new token
     return await prisma.refreshToken.create({
       data: {
         userId,
